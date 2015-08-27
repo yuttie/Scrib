12432781de21226518ad6b2beb4c71533fae798a
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-08-27 14:42:07 +0900

    Store a given text

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+extern crate crypto;
 extern crate docopt;
 extern crate rustc_serialize;
 
 use docopt::Docopt;
+use crypto::digest::Digest;
+use crypto::sha2::Sha256;
+use std::env;
+use std::fs::{self, File};
+use std::io;
+use std::io::prelude::*;
 
 const USAGE: &'static str = "
 Fine-grained.
 
 Usage:
-    fine add <text>...
+    fine add [<text>...]
     fine (-h | --help)
     fine --version
 
@@ -22,11 +29,50 @@ struct Args {
     arg_text: Vec<String>,
 }
 
+fn init() {
+    let mut pathbuf = env::home_dir().unwrap();
+
+    pathbuf.push(".fine");
+    fs::create_dir_all(pathbuf.as_path()).unwrap();
+
+    pathbuf.push("objects");
+    fs::create_dir_all(pathbuf.as_path()).unwrap();
+}
+
+fn add(text: &str) {
+    let digest = {
+        let mut hasher = Sha256::new();
+        hasher.input_str(&text);
+        hasher.result_str()
+    };
+
+    let mut file = {
+        let mut pathbuf = env::home_dir().unwrap();
+        pathbuf.push(".fine");
+        pathbuf.push("objects");
+        pathbuf.push(&digest);
+
+        File::create(&pathbuf).unwrap()
+    };
+    file.write_all(text.as_bytes()).unwrap();
+
+    println!("{}", &digest);
+}
+
 fn main() {
     let args: Args = Docopt::new(USAGE)
                             .and_then(|d| d.decode())
                             .unwrap_or_else(|e| e.exit());
+    init();
     if args.cmd_add {
-        println!("{}", args.arg_text.join(" "));
+        let text = if args.arg_text.is_empty() {
+            let mut buf = String::new();
+            io::stdin().read_to_string(&mut buf).unwrap();
+            buf
+        }
+        else {
+            args.arg_text.join(" ")
+        };
+        add(&text);
     }
 }
