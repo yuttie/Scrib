e747c49ce85725dffb8349f38c62d813469c68f5
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-09-10 12:38:35 +0900

    Fix a warning

--- src/main.rs
+++ src/main.rs
@@ -136,7 +136,7 @@ fn list() {
         let content = {
             let file = File::open(entry.path()).unwrap();
             let mut buf: Vec<u8> = Vec::new();
-            file.take(80).read_to_end(&mut buf);
+            file.take(80).read_to_end(&mut buf).unwrap();
             match String::from_utf8(buf.clone()) {
                 Ok(string) => string.lines().next().unwrap().to_owned(),
                 Err(_) => {
